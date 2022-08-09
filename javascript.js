fc204c310f5d92af066c7c1293d04e58ed099303
dimzles <dimzlestv@gmail.com>
2022-08-09 12:56:22 +0100

    Added score counting

--- javascript.js
+++ javascript.js
@@ -1,20 +1,47 @@
 const rock = "Rock";
 const paper = "Paper";
 const scissors = "Scissors";
-let playerSelection = prompt("rock, paper or scissors?");
+const container = document.querySelector('#container');
+const playerScore = document.getElementById('playerScore');
+const computerScore = document.getElementById('computerScore');
 
 
-function capitalise(str) { 
+const resultsContainer = document.createElement('div');
+const resultsDisplay = document.createElement('p');
+const scoreContainer = document.querySelector('#score-container');
+const gameOverContainer = document.createElement('div');
+const gameOverDisplay = document.createElement('p');
+
+scoreContainer.appendChild(gameOverContainer);
+container.appendChild(resultsContainer);
+resultsContainer.classList.add('results-container');
+resultsContainer.appendChild(resultsDisplay);
+resultsDisplay.textContent = "First to 5 wins!";
+gameOverContainer.appendChild(gameOverDisplay);
+
+function flCapital(str) { 
     if (typeof str !== 'string') return '';
     let str2 = str.slice(1)
     return str.charAt(0).toUpperCase() + str2.toLowerCase();
 }
 
+btn1.addEventListener('click', () => {
+    resultsDisplay.textContent = `${playRound(rock, computerPlay())}`
+})
+
+btn2.addEventListener('click', () => {
+    resultsDisplay.textContent = `${playRound(paper, computerPlay())}`
+})
+
+btn3.addEventListener('click', () => {
+    resultsDisplay.textContent = `${playRound(scissors, computerPlay())}`
+})
+
 //computerPlay func to randomly return rock/paper/scissors
 function computerPlay() {
     let computerChoice;
     let computerRandom = Math.random();
-
+    
     if (computerRandom < 0.34) {
         computerChoice = rock;
     } else if (computerRandom < 0.67 && computerRandom > 0.34) {
@@ -22,31 +49,47 @@ function computerPlay() {
     } else {
         computerChoice = scissors;
     }
-
+    
     return computerChoice;
 }
 
 //write func for a single round of r/p/s that takes playerSelection (case INsensitive)
 //and computerSelection as parameters, then returns a string to declare who won
-function playRound() {
-    let computerSelection = computerPlay();
-    let playerSelection2 = capitalise(playerSelection);
-    if (computerSelection === playerSelection2) {
-        return "It's a tie";
-    } else if (computerSelection === rock && playerSelection2 === scissors) {
-        return "Rock beats scissors, you lose.";
-    } else if (computerSelection === paper && playerSelection2 === rock) {
-        return "Paper beats rock, you lose.";
-    } else if (computerSelection === scissors && playerSelection2 === paper) {
-        return "Scissors beats paper, you lose.";
-    } else {
-        return `${playerSelection2} beats ${computerSelection}. You win!`;
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection === playerSelection) {
+        roundWinner = "It's a tie";
     }
+    
+    if (
+        (playerSelection === rock && computerSelection === scissors) ||
+        (playerSelection === scissors && computerSelection === paper) ||
+        (playerSelection === paper && computerSelection === rock)
+    ) {
+        playerScore.dataset.score = parseInt(playerScore.dataset.score) + 1;
+        playerScore.textContent = "Player: " + playerScore.dataset.score;
+        roundWinner = 'Player';
+    }
+    if (
+        (computerSelection === rock && playerSelection === scissors) ||
+        (computerSelection === scissors && playerSelection === paper) ||
+        (computerSelection === paper && playerSelection === rock)
+    ) {
+        computerScore.dataset.score = parseInt(computerScore.dataset.score) + 1;
+        computerScore.textContent = "Computer: " + computerScore.dataset.score;
+        roundWinner = 'Computer';
+    }
+    return "Player chooses: " + playerSelection +", Computer chooses: " + computerSelection + ", Round Winner: " + roundWinner + ".";
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound());
-        playerSelection = prompt("rock, paper or scissors?");
-    }
-}
+
+// !!!
+// function game() {
+//     while (playerScore || computerScore !== 5) {
+//         for (let i = 0; i < 5; i++) {
+//         }
+//     }
+//     if (playerScore === 5) {
+//         console.log(`You win! The score was ${playerScore} - ${computerScore}`)
+//     } else console.log(`You lose! The score was ${playerScore} - ${computerScore}`)
+// }
+// !!!
